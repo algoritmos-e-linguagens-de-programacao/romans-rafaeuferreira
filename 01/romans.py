949b12391484424ec8b4e7f9aec7c62af52f8e96
Rafael Ferreira <168040598+rafaeuferreira@users.noreply.github.com>
2024-06-18 21:26:34 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,59 +1,9 @@
-import tkinter as tk
-
-
-
 def int_to_roman(num):
-
     val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-
     syb = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
-
     roman_num = ''
-
     for i in range(len(val)):
-
         while num >= val[i]:
-
             roman_num += syb[i]
-
             num -= val[i]
-
     return roman_num
-
-
-
-def convert():
-
-    num = int(entry.get())
-
-    result.set(int_to_roman(num))
-
-
-
-root = tk.Tk()
-
-root.title("Calculadora de Números Romanos")
-
-
-
-entry = tk.Entry(root)
-
-entry.pack()
-
-
-
-button = tk.Button(root, text="Converter", command=convert)
-
-button.pack()
-
-
-
-result = tk.StringVar()
-
-label = tk.Label(root, textvariable=result)
-
-label.pack()
-
-
-
-root.mainloop()
